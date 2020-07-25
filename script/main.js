0818a9b53136a059ac792add63b46d6350dce859
Michael Ronald <ronaldmichael918@gmail.com>
2020-07-25 20:14:29 -0300

    Habilitando movimentacao da cobrinha e impedindo que ela suma da tela

--- script/main.js
+++ script/main.js
@@ -23,16 +23,23 @@ function criarSnake() {
 }
 
 function iniciarJogo() {
+    // Impedindo que a cobrinha desapareça da tela
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0
+    if (snake[0].x == 0 && direction == "left") snake[0].x = 16 * box
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
+
     criarBG()
     criarSnake()
-    // Movimento inicial da cobrinha
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
 
-    if (direction == "right") snakeX += box;
-    if (direction == "left") snakeX -= box;
-    if (direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
+    // Movimento inicial da cobrinha
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction == "right") snakeX += box
+    if (direction == "left") snakeX -= box
+    if (direction == "up") snakeY -= box
+    if (direction == "down") snakeY += box
 
     snake.pop()
 
@@ -44,4 +51,17 @@ function iniciarJogo() {
     snake.unshift(newHead)
 }
 
-let jogo = setInterval(iniciarJogo, 300)
+document.addEventListener('keydown', update)
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left"
+    if (event.keyCode == 38 && direction != "down") direction = "up"
+    if (event.keyCode == 39 && direction != "left") direction = "right"
+    if (event.keyCode == 40 && direction != "up") direction = "down"
+}
+
+let jogo = setInterval(iniciarJogo, 100)
+
+/* Bug
+    Nas quatro posições da tela é possível a cobrinha ficar em uma posição que suma da tela
+*/
